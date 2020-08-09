0bd7952c51355f2115a115bb973fe6ae4fce20f4
Ridwan Oseni <ridwanoseni101@gmail.com>
2020-08-08 17:05:02 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// Select the Elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+// Classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+// Show today's date
+const options = {weekday: "long", month: "short", day:"numeric"}
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+// add to do function
+function addToDo (toDo) {
+
+    const item = `
+                    <li class="item">
+                        <i class="fa fa-circle-thin co" job="complete" id="0"></i>
+                        <p class="text">${toDo}</p>
+                        <i class="fa fa-trash-o de" job="delete" id="0"></i>
+                    </li>
+                `;
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
+
+addToDo("Drink Coffee");
+
+
